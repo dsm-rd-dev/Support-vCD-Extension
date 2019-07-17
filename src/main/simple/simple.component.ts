130797dab417dfe564e5ba9f70018d3ca6eaf4f2
Grayton Ward <gward@dsm.net>
2019-07-17 14:24:53 -0400

    UI 2.0 with Bounds Check and Error Messages

--- src/main/simple/simple.component.ts
+++ src/main/simple/simple.component.ts
@@ -40,6 +40,10 @@ export class SimpleComponent implements OnInit {
     tenant: Observable < string > ;
     create: boolean;
     confirm: boolean;
+    inspect: boolean;
+
+    error: boolean;
+    errorMessage: string;
 
     ticketCreateData: object;
     ticketData: object;
@@ -64,7 +68,7 @@ export class SimpleComponent implements OnInit {
             this.create = false;
             console.log(this.createForm.value);
 
-            this.http.post("https://conheart.com/api/cw/ticket/", {
+            this.http.post(creds.url + '/cw/ticket/', {
                 "summary": this.createForm.get("org").value + "-" + this.createForm.get("user").value + ": " + this.createForm.get("summary").value,
                 "companyName": this.createForm.get("org").value,
                 "boardId": 6
@@ -102,7 +106,14 @@ export class SimpleComponent implements OnInit {
         this.create = false;
     }
 
+    openTicket(i:number){
+        this.inspect = true;
+        this.ticketData = this.companyData[i];
+        console.log(this.companyData[i]);
+    }
+
     getTickets() {
+        this.confirm = false;
         this.tenant.subscribe(
             data => {
                 this.http.get(creds.url + '/cw/company/' + data + '/lookup', {
@@ -111,6 +122,7 @@ export class SimpleComponent implements OnInit {
                     })
                 }).subscribe(
                     data => {
+                        this.companyId = data["id"];
                         this.http.get(creds.url + '/cw/company/' + data["id"] + '/tickets', {
                             headers: new HttpHeaders({
                                 'Authorization': creds.auth
@@ -133,11 +145,35 @@ export class SimpleComponent implements OnInit {
 
     }
 
+    fetchTicket(){
+        let id = this.getTicketForm.get("ticketID").value;
+        this.http.get(creds.url + '/cw/ticket/' + id, {
+            headers: new HttpHeaders({
+                'Authorization': creds.auth
+            })
+        }).subscribe(
+            data => {
+                console.log(data);
+                if(data["code"] == "EPARSE"){
+                    this.error = true;
+                    this.errorMessage = "Invalid Ticket ID";
+                }else if(data["company"]["id"] == this.companyId){
+                    this.inspect = true;
+                    this.ticketData = data;
+                }else{
+                    this.error = true;
+                    this.errorMessage = "Unauthorized to view that ticket";
+                }
+            }
+        )
+    }
+
     ngOnInit(): void {
         this.tenant = this.client.organization;
         this.username = this.client.username;
         this.create = false;
         this.confirm = false;
+        this.inspect = false;
         this.getTickets();
     }
 }
